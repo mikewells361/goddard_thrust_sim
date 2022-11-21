9a1288bb247cf46a9abfac16a7c952a34e51eb01
mikewells361 <mikewells361@gmail.com>
2022-11-21 11:31:18 -0600

    Update goddard_guide_law.py
    changed update() function to properly schedule the simulation, updated get_thrust() to properly manage thrust member

--- goddard_guide_law.py
+++ goddard_guide_law.py
@@ -19,6 +19,7 @@ class Rocket:
         self.dt = 1.              # time step, 60 Hz
         self.is_sing = False
         self.sing_tol = 1e-6
+        self.state = np.zeros([4, 1])
 
     def rocket_state(self, t, s):
         # rocket state equations
@@ -33,8 +34,6 @@ class Rocket:
         snew[1] = thrust/s[2] - d/s[2] - self.gravity
         snew[2] = -self.alpha*thrust
         snew[3] = thrust
-        # get drag
-        self.drag = self.calculate_drag(s)
         return snew
 
     def rk4(self, t, s):
@@ -54,68 +53,90 @@ class Rocket:
         return s + 1./6.*(k1 + 2.*k2 + 2.*k3 + k4)*self.dt
 
     # is the singularity thrust to be calculated?
-    def check_singularity(self, s):
-        drag = self.calculate_drag(s)
+    def check_singularity(self):
         self.is_sing = self.sing_tol >= \
-                        drag[0] + s[2]*self.gravity - \
-                        self.alpha*drag[0]*s[1] - \
-                        s[1]*drag[1]
+                        self.drag[0] + self.state[2]*self.gravity - \
+                        self.alpha*self.drag[0]*self.state[1] - \
+                        self.state[1]*self.drag[1]
 
     def get_thrust(self, s):
-        self.check_singularity(s)
+        #self.check_singularity(s)
         if self.is_sing:
             # update drag model.
-            drag = self.calculate_drag(s)
-            d = drag[0]
-            dv = drag[1]
-            ddv = drag[2]
-            ddh = drag[3]
+            #drag = self.calculate_drag(s)
+            d = self.drag[0]
+            dv = self.drag[1]
+            ddv = self.drag[2]
+            ddh = self.drag[3]
             ddvdh = 0.  # update from drag terms with updated model
             p1 = s[2]/(d + 2.*self.c*dv+self.c**2*ddv)
             p2 = -self.gravity*(d+self.c*dv) #+self.c*(self.c-s[1])*ddh-s[1]*self.c**2*ddvdh
             #thrust = d + s[2]*self.gravity + p1*p2
             thrust = d + s[2]*self.gravity + s[2]/(d+2*self.c*dv+self.c**2*ddv)*-self.gravity*(d+self.c*dv)
+            thrust = float(thrust)  # remove warning 
         else:
             thrust = self.thrust_max
+        if thrust > self.thrust_max:
+            raise Exception("THRUST CANNOT BE BIGGER THAN THRUST_MAX!!!")
         return thrust
 
-    def calculate_drag(self, s):
+    def calculate_drag(self):
         # simple drag model that should be updated for complexity--this mirrors the model used in ECE6570
         drag =  np.array([0., 0., 0., 0.])
-        drag[0] = 0.5*s[1]**2
-        drag[1] = s[1]
+        drag[0] = 0.5*self.state[1]**2
+        drag[1] = self.state[1]
         drag[2] = 1.
-        drag[3] = s[0]
+        drag[3] = self.state[0]
         return drag
 
+    def set_state(self):
+        self.state[0] = self.height
+        self.state[1] = self.velocity
+        self.state[2] = self.mass
+        self.state[3] = self.thrust
+
+    def set_state_params(self):
+        self.height = self.state[0]
+        self.velocity = self.state[1]
+        self.mass = self.state[2]
+        self.thrust = self.state[3]
+
+    # schedule updates to state, drag, etc.
     def update(self):
-        self.height, self.velocity, self.mass, self.thrust = self.rk4(self.time, np.array([self.height, self.velocity, self.mass, self.thrust]))
+        # update drag and singularity calculations first
+        self.drag = self.calculate_drag()
+        # check singularity
+        self.check_singularity()
+        self.height, self.velocity, self.mass, dummy = self.rk4(self.time, np.array([self.height, self.velocity, self.mass, self.thrust]))
+        self.thrust = self.get_thrust(self.state) # uncumbered by rk4 algorithm
+        # update state params
+        self.set_state()
 
 
 
 def main():
     rocket = Rocket()
+    rocket.set_state() # initial parameters set to state
     tsav = []
     thsav = []
     hsav = []
-    count = 0
-    while rocket.mass >= rocket.mass_min and count <= 1000:
+    sing = []
+    while rocket.mass >= rocket.mass_min:
         # print(rocket.mass)
         rocket.update()
         # print(rocket.mass)
         tsav.append(rocket.time)
         thsav.append(rocket.thrust)
         hsav.append(rocket.height)
-        # if count > 1000:
-        #     break
-        count += 1
+        sing.append(rocket.is_sing)
     np.array(tsav)
     np.array(thsav)
     np.array(hsav)
-    plt.figure()
+    np.array(sing)
+    plt.figure("Thrust v time")
     plt.plot(tsav, thsav)
     plt.show()
-    plt.figure()
+    plt.figure("height v time")
     plt.plot(tsav, hsav)
     plt.show()
 
